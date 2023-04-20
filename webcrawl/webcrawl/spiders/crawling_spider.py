b13c1bacd5a414807adfda734d52cac2c37a02cc
FengchunFan <ffan005@ucr.edu>
2023-04-20 00:25:15 -0700

    still need to work on library and extension issue

--- webcrawl/webcrawl/spiders/crawling_spider.py
+++ webcrawl/webcrawl/spiders/crawling_spider.py
@@ -13,12 +13,12 @@ class WebCrawler(CrawlSpider):
     #    start_urls.append("https://news.ucr.edu/articles?page=" + str(i))
 
     #size limiter
-    max_size = 0.5 * 1024 * 1024 #0.5MB => 512KB
+    max_size = 0.2 * 1024 * 1024 #1MB
     current_size = 0
 
     #crawl
     rules = (
-        Rule(LinkExtractor(allow=".edu", deny="extension.ucr.edu/course/" and "collections/overview"), callback="parse"),
+        Rule(LinkExtractor(allow=".edu"), callback="parse"), #can't figure these out yet
     #    Rule(LinkExtractor(allow="articles"), callback="parse"),
     #    Rule(LinkExtractor(allow="about"), callback="parse"),
     #    Rule(LinkExtractor(allow="research"), callback="parse"),
@@ -26,13 +26,17 @@ class WebCrawler(CrawlSpider):
 
     #decide how to scrape the links
     def parse(self, response):
+        #force return type to be text, avoid scrapy.exceptions.NotSupported: Response content isn't text, chatgpt generated code
+        content_type = response.headers.get('content-type', '').decode('utf-8').lower()
+        if 'text/html' not in content_type:
+            return
         #specified by check source code for ucr webs
         title = response.css('meta[property="og:title"]::attr(content)').get() 
         description = response.css('meta[property="og:description"]::attr(content)').get()
         url = response.css('meta[property="og:url"]::attr(content)').get()
         # if none of the three field yield null and size is less than max_size
         if title and description and url and (self.current_size <= self.max_size): 
-            temp_size = len(title) + len(description) + len(url) + len("title:") + len("description:") + len("url:")
+            temp_size = len(title) + len(description) + len(url)
             self.current_size = self.current_size + temp_size
             yield{
                 "title": title,
