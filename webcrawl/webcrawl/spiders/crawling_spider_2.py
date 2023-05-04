c7dbe48f9b6fb072b51a84c3b336975a94a501d1
FengchunFan <ffan005@ucr.edu>
2023-05-03 21:47:28 -0700

    hotel crawling?

--- webcrawl/webcrawl/spiders/crawling_spider_2.py
+++ webcrawl/webcrawl/spiders/crawling_spider_2.py
@@ -1,7 +1,4 @@
-#reference: https://www.youtube.com/watch?v=m_3gjHGxIJc
-
-#compile line: scrapy crawl WebCrawler
-#size check: stat -c "%s" output0.csv
+#scrapy crawl HotelCrawler -o output.json
 
 import sys
 import os
@@ -10,18 +7,12 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.exceptions import CloseSpider #stop spider under certain condition
 
-class WebCrawler(CrawlSpider):
-    name = "WebCrawler" #identifier
-    allowed_domains = [".edu"]
+class HotelCrawler(CrawlSpider):
+    name = "HotelCrawler" #identifier
+    allowed_domains = ["www.hotels.com"]
     #allowed_domains = [".edu", ".com"]
     #seed.txt, to be modified
-    start_urls = ["https://www.ucr.edu/", "https://ucsd.edu/", "https://www.ucmerced.edu/", "https://uci.edu/", "https://www.ucdavis.edu/", "https://www.ucsc.edu/", "https://www.ucsb.edu/", "https://www.berkeley.edu/", "https://www.ucla.edu/"]
-
-    #size limiter
-    max_size = 2 * 1024 * 1024 #10MB
-    #current_size = 0
-    document_num = 0
-    file_name = f"output{document_num}.csv"
+    start_urls = ["https://www.hotels.com/"]
     
     #crawl
     rules = (
@@ -66,17 +57,6 @@ class WebCrawler(CrawlSpider):
                 writer = csv.writer(file)
                 writer.writerow([item.replace('\n', '') for item in data]) #keep everything in 1 page 1 row format
                 file.close()
-
-        #size checker, making sure each output contains only the limited amount of data
-        if(os.path.isfile(self.file_name)):
-            if(os.path.getsize(self.file_name) > self.max_size):
-                print("reach size limit, size of output file: ", os.path.getsize(self.file_name)/1024/1024, "MB")
-                self.document_num += 1
-                self.file_name = f"output{self.document_num}.csv"
-                #raise CloseSpider
-
-        if(self.document_num == 13):
-            raise CloseSpider
 
         #reference: https://www.youtube.com/watch?v=-mkewdn9JdU&t=415s
         for link in response.css('a::attr(href)').getall():
